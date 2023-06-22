8612e06080c69f31caf68cb23ee344f219a474e9
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-22 12:27:28 +0100

    created show route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,20 @@ class UserController < ApplicationController
         @users = User.all
     end
 
-    
+    def show
+        @user=User.find(params[:id])
+    end
+
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
+
+
+
 
 end

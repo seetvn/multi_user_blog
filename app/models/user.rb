470525ce99fa3bb33c0d9d50fe9319992fce36c3
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-22 21:33:20 +0100

    users can be created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true, length: { minimum: 5, maximum: 20 }
+    validates :email, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "user#index"
-  resources :user
+  root "users#index"
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

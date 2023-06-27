cc46d3ecf919322b34f1867a9bc846ca50912091
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-27 11:47:14 +0100

    added password in params + edited form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :name, presence: true, length: { minimum: 5, maximum: 20 }, uniqueness: true
     validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: {minimum: 5}
 end
